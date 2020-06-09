5671eae5f7fee1d9fa5a90684fbe0d4a7077ed10
JimmyYang <jimmyyang886@gmail.com>
2020-06-09 18:16:00 +0800

    Add files via upload
    upload for time interval filling~~

--- static/js/post.js
+++ static/js/post.js
@@ -1,5 +1,6 @@
 function getData() {
             var chart = echarts.init(document.getElementById('kline'), 'white', {renderer: 'canvas'});
+
             $.ajax({
                 type: "POST",
                 dataType: "json",
@@ -7,9 +8,22 @@ function getData() {
                 data: $('#form1').serialize(),
                 success: function (result) {
                     chart.setOption(result);
-                },
+					chart.on('click', function (result) {
+						//alert(result.name);
+						var click_time = result.name;
+						var start_time = time_interval.start.value;
+						//click_time.replace(/-/g, "/");
+						//start_time.replace(/-/g, "/");
+						if (Date.parse(click_time).valueOf() > Date.parse(start_time).valueOf()){
+							time_interval.end.value = click_time;
+						} else{
+							time_interval.start.value = click_time;
+					    }
+				    });
+	             },
                 error: function() {
-                    alert("	wrong stock code！");
-                }
+					alert("	wrong stock code！");
+					}
             });
         }
+		
