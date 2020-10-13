86a0a95dc19f00da75fe68211d95d5f891956579
JimmyYang <jimmyyang886@gmail.com>
2020-10-13 22:56:40 +0800

    Update TWSE_STOCK_Transactions.py
    for airflow

--- TWSE_STOCK_Transactions.py
+++ TWSE_STOCK_Transactions.py
@@ -26,6 +26,6 @@ TWSE_Fetch_operator = BashOperator(task_id='TWSE_FETCH_task',
         bash_command='/home/spark/PycharmProjects/Stock_Price_API/FetchTWSEprice_daily.sh ', dag=dag)
 
 TWSE_Import_operator = BashOperator(task_id='TWSE_import_task', 
-        bash_command='/home/saprk/PycharmProjects/Stock_Price_API/ImportTWSEprice_daily.sh ', dag=dag)
+        bash_command='/home/spark/PycharmProjects/Stock_Price_API/ImportTWSEprice_daily.sh ', dag=dag)
 
 TWSE_Fetch_operator >> TWSE_Import_operator  
